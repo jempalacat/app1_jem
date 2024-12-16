79e41ab08f43f43bb5b92f6f40e790fe36e5e94d
jempalacat <121996263+jempalacat@users.noreply.github.com>
2024-12-15 23:21:02 -0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
   define('DB_SERVER', 'localhost');
-  define('DB_USERNAME', 'root');
-  define('DB_PASSWORD', '');
+  define('DB_USERNAME', 'jem');
+  define('DB_PASSWORD', 'jem');
   define('DB_NAME', 'student');
 
   $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
